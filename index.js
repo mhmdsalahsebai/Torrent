c2b84d4bb107684dd24eb3784f475b64830f9a78
Mohamed Salah <mhmdsalahsebai@gmail.com>
2024-01-01 10:07:21 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const tracker = require("./tracker");
+const torrentParser = require("./torrent-parser");
+
+const torrent = torrentParser.open("puppy.torrent");
+
+tracker.getPeers(torrent, (peers) => {
+  console.log("list of peers", peers);
+});
